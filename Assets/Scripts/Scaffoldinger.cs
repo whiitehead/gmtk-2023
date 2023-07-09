ce7a02ef0957553aeec2468d8a10ffcd4dc387b5
Caleb Whitehead <whiitehead@gmail.com>
2023-07-09 09:35:10 -0700

    dondonodjneondeon

---
+++ Assets/Scripts/Scaffoldinger.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Scaffoldinger : MonoBehaviour
+{
+    private AudioSource src;
+    
+    void Start()
+    {
+        src = GetComponent<AudioSource>();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Awake()
+    {
+        src.Play();
+    }
+}
