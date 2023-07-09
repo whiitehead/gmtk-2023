f9dca0f32f5453ff47b830c8c1b045998890c67b
Caleb Whitehead <whiitehead@gmail.com>
2023-07-08 17:41:51 -0700

    add max wait ticks

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -15,13 +15,27 @@ public class Entity : MonoBehaviour
 
     public Vector3Int pos;
     public Vector3Int dir;
+    [HideInInspector]
+    public int maxWaitTicks;
     public int waitTicks = 3;
     public int waitTicksCount = 0;
-    public int speed = 1;
-    
-
     public int health;
     public int maxHealth;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        maxWaitTicks = waitTicks;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        spriteRenderer.flipX = dir != Vector3Int.right;
+
+        transform.Translate((pos - transform.position) * GameBoard.tickWaitTime * Time.deltaTime);
+    }
 
     private void OnDrawGizmos()
     {
